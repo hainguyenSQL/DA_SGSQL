8dd54801890d346ae71275fd426281fc60c15f75
hainguyenSQL <shirushi754@gmail.com>
2025-03-27 23:31:52 +0700

    Update practice_3.sql

--- practice_3.sql
+++ practice_3.sql
@@ -32,3 +32,35 @@ extract (year from sent_date) = 2022
 GROUP BY sender_id
 ORDER BY message_count DESC
 limit 2
+--ex6
+select tweet_id
+from tweets
+where length(content) > 15 
+--ex7
+select
+activity_date as day,
+count(distinct(user_id)) as active_users
+from activity
+where activity_date between '2019-06-28' and '2019-07-27'
+group by activity_date
+--ex8 
+select
+count(distinct(id)) as employees
+from employees
+where
+extract(month from joining_date) between 1 and 7 and
+extract(year from joining_date) = 2022
+group by joining_date
+--ex9 
+select
+first_name,
+position('a' in first_name) as position
+from worker
+where first_name = 'Amitah'
+--ex10
+SELECT 
+    title, 
+   REGEXP_SUBSTR(title, '[0-9]{4}') AS vintage_year
+FROM winemag_p2
+WHERE country = 'Macedonia';
+--REGEXP_SUBSTR(title, '[0-9]{4}') → Extracts the first 4-digit number (which represents the year) from the title column.
